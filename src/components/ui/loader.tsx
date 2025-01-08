3448f1aa3c1288647d1449577621bb75d6ebd52b
cod3prod <cod3prod@gmail.com>
2025-01-08 12:38:45 +0900

    💄 Styling Loader component

--- src/components/ui/loader.tsx
+++ src/components/ui/loader.tsx
@@ -1,12 +1,30 @@
-import { twMerge } from "tailwind-merge";
+import { useEffect, useState } from "react";
 
-export default function Loader({ className }: { className?: string }) {
+export default function Loader() {
+  const [colors, setColors] = useState(["bg-primary-450", "bg-primary-400", "bg-primary-350"]);
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setColors((prevColors) => {
+        const [first, ...rest] = prevColors;
+        return [...rest, first];
+      });
+    }, 1000);
+
+    return () => clearInterval(interval);
+  }, []);
+
   return (
-    <div
-      className={twMerge(
-        "w-8 h-8 border-4 border-t-4 border-blue-600 border-solid rounded-full animate-spin",
-        className
-      )}
-    ></div>
+    <div className="fixed inset-0 z-50 flex items-center justify-center h-screen bg-black/20">
+      <div className="flex space-x-2">
+        {colors.map((color, index) => (
+          <div
+            key={index}
+            className={`w-4 h-4 ${color} rounded-full animate-bounce transition-colors duration-1000`}
+            style={{ animationDelay: `${index * 0.2}s` }}
+          ></div>
+        ))}
+      </div>
+    </div>
   );
 }
